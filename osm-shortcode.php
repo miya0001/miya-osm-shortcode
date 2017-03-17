7fc57342af26c316f5df6cd73c6648f87d16f75c
miya0001 <miya@wpist.me>
2017-03-17 15:08:38 +0900

    first commit

---
+++ osm-shortcode.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Osm Shortcode
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     osm-shortcode
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Osm_Shortcode
+ */
